e65f16107c628f44fe297eb77d283751741971da
Anthony Quivers <anthonyquivers@gmail.com>
2018-12-25 09:31:47 -0500

    urls and database for polls and site respectively

--- votefire/votefire/settings.py
+++ votefire/votefire/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'votefire.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'votefire',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['POSTGRES_IN'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
