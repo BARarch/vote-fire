e956d23233628e16641274d902c49c488cf0f144
Anthony Quivers <anthonyquivers@gmail.com>
2019-01-12 17:11:24 -0500

    You should know basic math before using a calculator

--- votefire/polls/views.py
+++ votefire/polls/views.py
@@ -7,13 +7,19 @@ from .models import Choice, Question
 
 # Create your views here.
 class IndexView(generic.ListView):
-    pass
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-    pass
+    model = Question
+    template_name = 'polls/detail.html'
 
 class ResultsView(generic.DetailView):
-    pass
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
